535c3258dffcea9f684a36fe876acc6802d0725e
herry081288 <herry081288@gmail.com>
2024-12-02 14:22:50 +0000

    addPage

---
+++ lib/addpage.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:myapp/homepage.dart';
+
+class AddNotePage extends StatefulWidget {
+  const AddNotePage({super.key});
+
+  @override
+  State<AddNotePage> createState() => _AddNotePageState();
+}
+
+class _AddNotePageState extends State<AddNotePage> {
+  // final String? uid = user?.uid;
+  final TextEditingController _noteController = TextEditingController();
+
+  Future<void> _addNote(String content) async {
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      print('User Belum Login');
+      return;
+    }
+    final String uid = user!.uid;
+    try {
+      await FirebaseFirestore.instance.collection('notes').add({
+        'content': _noteController.text,
+        'timestamp': Timestamp.now(),
+        'userId': uid
+      });
+      print('Catatan berhasil ditambahkan untuk UID $uid');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Catatan berhasil ditambahkan')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Terjadi kesalahan saat menambahkan catatan: $e'),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tambah Catatan'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _noteController,
+              decoration: InputDecoration(labelText: 'Isi Catatan'),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _addNote();
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
+              child: Text('Simpan'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
